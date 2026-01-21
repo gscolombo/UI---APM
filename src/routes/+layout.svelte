63ccb602968a32b9882c642170acd7cc904eddc6
Gabriel de Souza Colombo <gscolombo404@gmail.com>
2026-01-20 21:08:53 -0300

    feat: rodapé do site

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import type { LayoutProps } from './$types';
   import favicon from '$lib/assets/favicon.svg';
+  import spain from '$lib/assets/spain.png';
 
   import '../reset.css';
   import '../app.css';
@@ -9,11 +10,9 @@
   let showMobileMenu = $state(false);
 
   $effect(() => {
-    if (showMobileMenu)
-      document.body.style.overflowY = 'hidden';
-    else
-      document.body.style.overflowY = '';
-  })
+    if (showMobileMenu) document.body.style.overflowY = 'hidden';
+    else document.body.style.overflowY = '';
+  });
 </script>
 
 <!-- <head> tag to be shown in every page of the app -->
@@ -42,6 +41,10 @@
     rel="stylesheet"
     href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css"
   />
+  <script
+    src="https://kit.fontawesome.com/80d53e6a74.js"
+    crossorigin="anonymous"
+  ></script>
 </svelte:head>
 
 <!-- navigation bar -->
@@ -113,6 +116,39 @@
   </div>
 </section>
 
+<footer>
+  <div class="content">
+    <div class="disclaimer">
+      <img src={data.siteLogoURL} alt="" />
+      <p>
+        Este site contém informações e materiais de caráter exclusivamente
+        informativo e não devem ser interpretados como aconselhamento jurídico.
+        O uso do site não cria relação advogada-cliente.
+      </p>
+    </div>
+    <div class="contacts">
+      <h1>Contatos</h1>
+      <ul>
+        <li><i class="bi bi-whatsapp"></i>phone_number</li>
+        <li><i class="bi bi-envelope-at-fill"></i>email</li>
+      </ul>
+    </div>
+    <div class="accessibility">
+      <h1>Atendimento acessível</h1>
+      <ul>
+        <li><i class="fa-solid fa-hands"></i>LIBRAS</li>
+        <li><img src={spain} alt="Bandeira da Espanha" />ESPANHOL</li>
+        <li>Atendimento em todo o Brasil.</li>
+        <li>Flagrantes 24h no DF e entorno.</li>
+      </ul>
+    </div>
+  </div>
+  <p class="copyright">
+    &copy; Todos os direitos reservados - Ana Paula Miranda Silva | Advogada
+    Criminal - {new Date().getFullYear()}
+  </p>
+</footer>
+
 <style lang="scss">
   $enable-important-utilities: false;
 
@@ -126,6 +162,11 @@
     top: 0;
     width: 100vw;
     z-index: 10;
+    border-bottom: 2.5px solid var(--primary-red);
+
+    img {
+      max-height: 72px;
+    }
 
     .buttons {
       display: flex;
@@ -268,6 +309,86 @@
           }
         }
       }
+    }
+  }
+
+  footer {
+    background-color: var(--secondary-brown);
+    border-top: 10px solid var(--primary-brown);
+
+    .content {
+      display: grid;
+      grid-template-columns: repeat(3, 1fr);
+      padding: 1rem 2rem 2rem 2rem;
+      gap: 10rem;
+
+      .disclaimer {
+        display: flex;
+        flex-direction: column;
+        gap: 10px;
+
+        img {
+          max-height: 72px;
+          align-self: flex-start;
+        }
+
+        p {
+          color: var(--primary-white);
+          font-weight: 700;
+          font-size: 1.125rem;
+        }
+      }
+
+      .contacts,
+      .accessibility {
+        display: flex;
+        flex-direction: column;
+        gap: 10px;
+
+        h1 {
+          font-size: 1.5rem;
+          font-weight: 700;
+          color: var(--primary-red);
+        }
+
+        ul {
+          display: flex;
+          flex-direction: column;
+          gap: 20px;
+          padding: 0;
+
+          li {
+            display: flex;
+            align-items: center;
+            gap: 10px;
+            font-size: 1.25rem;
+            color: var(--primary-white);
+            font-weight: 700;
+
+            &:nth-of-type(3n) {
+              color: var(--primary-red);
+            }
+            &:nth-of-type(4n) {
+              margin-top: -10px;
+              color: var(--primary-red);
+            }
+          }
+        }
+      }
+
+      .accessibility ul li img {
+        max-height: 24px;
+      }
+    }
+
+    .copyright {
+      margin: 0;
+      text-align: center;
+      padding: 1rem 2rem;
+      background-color: var(--primary-brown);
+      color: var(--primary-red);
+      font-size: 1rem;
+      font-weight: 700;
     }
   }
 
