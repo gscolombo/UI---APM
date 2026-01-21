c820e33a32dced6c858f529784ef1c40486b2121
Gabriel de Souza Colombo <gscolombo404@gmail.com>
2026-01-21 18:26:31 -0300

    refactor: chamada única para API do CMS

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -2,12 +2,44 @@ import type { Actions, LayoutServerLoad } from "./$types";
 
 import { STRAPI_SERVER_URL } from "$env/static/private";
 import { client } from "$lib/strapiClient";
+import type Review from "$lib/interfaces/Review";
+import type ActingArea from "$lib/interfaces/ActingArea";
 
 export const load: LayoutServerLoad = async () => {
-    const { siteName, siteLogo } = (await client.single('global').find({ populate: "*" })).data;
+
+    const simpleFields = ['siteName']
+
+    const populate = [
+        'siteLogo',
+        'about', 'about.profile',
+        'googleReviews', 'googleReviews.reviews',
+        'actingAreas', 'actingAreas.image'
+    ]
+
+    const { siteName, siteLogo, about, googleReviews, actingAreas } = (await client.single('global').find({ populate: populate, fields: simpleFields })).data;
+
+    // Parse publish datetime and get only initials of every review author
+    const reviews: Review[] = (googleReviews.reviews as Review[]).map((review) => {
+        review.publishTime = new Date(review.publishTime).toLocaleDateString('pt-BR');
+        review.author = review.author.split(" ").map((s): string => s.at(0)!).join(".");
+        return review;
+    })
+
+    const _actingAreas: ActingArea[] = (actingAreas as any[]).map((item) => (
+        {
+            title: item.title,
+            description: item.description,
+            imgSrc: STRAPI_SERVER_URL + item.image.formats.small.url
+        }
+    ))
 
     return {
         siteName: siteName,
-        siteLogoURL: STRAPI_SERVER_URL + siteLogo.url
+        siteLogoURL: STRAPI_SERVER_URL + siteLogo.url,
+        introductionText: about.introductionText,
+        profile: STRAPI_SERVER_URL + about.profile.url,
+        averageRating: googleReviews.averageRating,
+        reviews: reviews,
+        actingAreas: _actingAreas
     };
 };
--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,42 +1,7 @@
-import type { Actions, PageServerLoad } from './$types';
+import type { Actions } from './$types';
 
-import { STRAPI_SERVER_URL } from "$env/static/private";
 import { client } from '$lib/strapiClient';
-import type Review from '$lib/interfaces/Review';
-import type ActingArea from '$lib/interfaces/ActingArea';
 import { fail } from '@sveltejs/kit';
-
-export const load: PageServerLoad = async () => {
-    const toPopulate = [
-        'about', 'about.profile',
-        'googleReviews', 'googleReviews.reviews',
-        'actingAreas', 'actingAreas.image'
-    ]
-    const { about, googleReviews, actingAreas } = (await client.single('global').find({ populate: toPopulate })).data;
-
-    // Parse publish datetime and get only initials of every review author
-    const reviews: Review[] = (googleReviews.reviews as Review[]).map((review) => {
-        review.publishTime = new Date(review.publishTime).toLocaleDateString('pt-BR');
-        review.author = review.author.split(" ").map((s): string => s.at(0)!).join(".");
-        return review;
-    })
-
-    const _actingAreas: ActingArea[] = (actingAreas as any[]).map((item) => (
-        {
-            "title": item.title,
-            "description": item.description,
-            "imgSrc": STRAPI_SERVER_URL + item.image.formats.small.url
-        }
-    ))
-
-    return {
-        "introductionText": about.introductionText,
-        "profile": STRAPI_SERVER_URL + about.profile.url,
-        "averageRating": googleReviews.averageRating,
-        "reviews": reviews,
-        "actingAreas": _actingAreas
-    }
-}
 
 export const actions = {
     contact: async ({ request }) => {
